58b4c08ac7578edb08e7fd1c5bfb7dd66db9a4c9
Tiago Chaves <tchaves74@gmail.com>
2022-03-02 00:34:56 -0300

    making func as public

--- Sources/RazeCore/RazeCore.swift
+++ Sources/RazeCore/RazeCore.swift
@@ -7,7 +7,7 @@ public class RazeCore {
     /// - Parameters:
     ///   - hexString: A 6-digit hexadecimal string. Use 6 digits rather than 8, and add the accompany alpha value in the second parameter.
     ///   - alpha: A number between 0.0 and 1.0 indicating how transparent the color is.
-    static func colorFromHexString(_ hexString: String, alpha: CGFloat = 1.0) -> UIColor {
+    public static func colorFromHexString(_ hexString: String, alpha: CGFloat = 1.0) -> UIColor {
         let r, g , b: CGFloat
         let offset = hexString.hasPrefix("#") ? 1 : 0
         let start = hexString.index(hexString.startIndex, offsetBy: offset)
