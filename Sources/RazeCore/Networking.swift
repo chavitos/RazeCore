85ce09e3383f0baf018426954d2bee05d2b035d7
Tiago Chaves <tchaves74@gmail.com>
2022-03-28 19:37:42 -0300

    Add networking class

---
+++ Sources/RazeCore/Networking.swift
@@ -0,0 +1,14 @@
+//
+//  Networking.swift
+//  RazeCore
+//
+//  Created by Tiago Chaves on 28/03/22.
+//
+
+import Foundation
+
+extension RazeCore {
+    public class Networking {
+        
+    }
+}
---
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -0,0 +1,19 @@
+//
+//  RazeNetworkingTests.swift
+//  RazeCoreTests
+//
+//  Created by Tiago Chaves on 28/03/22.
+//
+
+import XCTest
+
+final class RazeNetworkingTests: XCTestCase {
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+}
