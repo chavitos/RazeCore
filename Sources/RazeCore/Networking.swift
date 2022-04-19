f40e2612bbff67745a45bf98f7d3819fdaf49318
Tiago Chaves <tchaves74@gmail.com>
2022-04-18 21:52:37 -0300

    Added second Networking API for sending data

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -9,11 +9,19 @@ import Foundation
 
 protocol NetworkSession {
     func get(from url: URL, completion: @escaping ((Data?, Error?) -> Void))
+    func post(with request: URLRequest, completion: @escaping ((Data?, Error?) -> Void))
 }
 
 extension URLSession: NetworkSession {
     func get(from url: URL, completion: @escaping ((Data?, Error?) -> Void)) {
         let task = dataTask(with: url) { data, _, error in
+            completion(data, error)
+        }
+        task.resume()
+    }
+    
+    func post(with request: URLRequest, completion: @escaping ((Data?, Error?) -> Void)) {
+        let task = dataTask(with: request) { data, _, error in
             completion(data, error)
         }
         task.resume()
@@ -30,15 +38,36 @@ extension RazeCore {
             
             internal var session: NetworkSession = URLSession.shared
             
-            
             /// Calls to the live internet to retrieve Data from a scpecific location
             /// - Parameters:
             ///   - url: The location you wish to fetch data from
             ///   - completionHandler: Returns a result object which signifies the status of the request
-            public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+            public func loadData(from url: URL, completion: @escaping (NetworkResult<Data>) -> Void) {
                 session.get(from: url) { data, error in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
-                    completionHandler(result)
+                    completion(result)
+                }
+            }
+            
+            
+            /// Calls to the live internet to send data to a specific location
+            /// - Warning: Make sure that the URL in question can accept a POST route
+            /// - Parameters:
+            ///   - body: The object you wish to send over the network
+            ///   - url: The location you wish to send data to
+            ///   - completion: Returns a result object which signifies the status of the request
+            public func send<I: Codable>(_ body: I, to url: URL, completion: @escaping (NetworkResult<Data>) -> Void) {
+                var request = URLRequest(url: url)
+                do {
+                    let body = try JSONEncoder().encode(body)
+                    request.httpBody = body
+                    request.httpMethod = "POST"
+                    session.post(with: request) { data, error in
+                        let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                        completion(result)
+                    }
+                } catch let error {
+                    return completion(.failure(error))
                 }
             }
         }
--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -15,6 +15,15 @@ class NetworkSessionMock: NetworkSession {
     func get(from url: URL, completion: @escaping ((Data?, Error?) -> Void)) {
         completion(data, error)
     }
+    
+    func post(with request: URLRequest, completion: @escaping ((Data?, Error?) -> Void)) {
+        completion(data, error)
+    }
+}
+
+struct MockData: Codable, Equatable {
+    var id: Int
+    var name: String
 }
 
 final class RazeNetworkingTests: XCTestCase {
@@ -36,6 +45,36 @@ final class RazeNetworkingTests: XCTestCase {
                 XCTAssertEqual(data, returnedData, "manager returned unexpected data")
             case .failure(let error):
                 XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    func testSendDataCall() {
+        let session = NetworkSessionMock()
+        let manager = RazeCore.Networking.Manager()
+        
+        let sampleObject = MockData(id: 1, name: "David")
+        guard let data = try? JSONEncoder().encode(sampleObject) else {
+            XCTFail("Error trying to encode sample object")
+            return
+        }
+        
+        session.data = data
+        manager.session = session
+        
+        let url = URL(fileURLWithPath: "url")
+        let expectation = XCTestExpectation(description: "Sent data")
+        
+        manager.send(data, to: url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let data):
+                let returnedData = try? JSONDecoder().decode(MockData.self, from: data)
+                XCTAssertEqual(returnedData, sampleObject)
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "Error forming error result")
             }
         }
         
